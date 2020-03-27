9c0b7bd5481295d1cbb37667491e26451db448c9
Phantasye <quintonk.94@gmail.com>
2020-03-27 16:31:15 -0500

    Idle Logout

--- src/ethos/model/players/packets/IdleLogout.java
+++ src/ethos/model/players/packets/IdleLogout.java
@@ -2,25 +2,27 @@ package ethos.model.players.packets;
 
 import ethos.model.players.PacketType;
 import ethos.model.players.Player;
+import ethos.util.Misc;
 
 public class IdleLogout implements PacketType {
 
 	@Override
 	public void processPacket(Player c, int packetType, int packetSize) {
-//		if (c.underAttackBy > 0) {
-//			return;
-//		}
-//		try {
-		if (!c.isIdle) {
-			if (c.debugMessage)
-				c.sendMessage("You are now in idle mode.");
-			c.isIdle = true;
+		System.out.println("LOGOUT");
+		if (c.underAttackBy > 0) {
+			return;
 		}
-//			c.logout();
-//			c.disconnected = true;
-//			Misc.println(c.playerName + " is idle, kicked.");
-//		} catch (Exception e) {
-//			e.printStackTrace();
+		try {
+//		if (!c.isIdle) {
+//			if (c.debugMessage)
+//				c.sendMessage("You are now in idle mode.");
+//			c.isIdle = true;
 //		}
+			c.logout();
+			c.disconnected = true;
+			Misc.println(c.playerName + " is idle, kicked.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }

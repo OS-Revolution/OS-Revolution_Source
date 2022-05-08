e10043f7f9142229160dd3246994f6ad332da060
Phantasye <quintonk.94@gmail.com>
2022-05-08 12:08:07 -0600

    Updated tutorial to be repeatable, added exchange slot limit

--- src/ethos/runehub/action/click/npc/FirstClickNpcListener.java
+++ src/ethos/runehub/action/click/npc/FirstClickNpcListener.java
@@ -2,11 +2,15 @@ package ethos.runehub.action.click.npc;
 
 import ethos.model.players.Player;
 import ethos.runehub.action.click.npc.impl.FirstClickEstateAgentAction;
+import ethos.runehub.action.click.npc.impl.FirstClickGuideAction;
+import ethos.runehub.entity.mob.passive.GuideNpcUtils;
 
 public class FirstClickNpcListener {
 
     public static ClickNpcAction onClick(Player player, int npcId, int npcIndex) {
         switch (npcId) {
+            case GuideNpcUtils.NPC_ID:
+                return new FirstClickGuideAction(player,npcId,npcIndex);
             case 5419:
                 return new FirstClickEstateAgentAction(player,npcId,npcIndex);
             default: throw new NullPointerException("Nothing interesting happens.");
